46e2548c571d75466ff00bdde5aac0fe89e97adc
Hyunwoo Lee <hw5773@gmail.com>
2024-12-04 23:36:36 +0900

    update the pascal triangle

--- 8/pascal.py
+++ 8/pascal.py
@@ -3,13 +3,12 @@ import argparse
 import logging
 from counting import combination
 
-def pascal_triangle(n, k):
+def pascal_triangle(n):
     pass
 
 def command_line_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-n", "--number", required=True, metavar="<total level of a pascal triangle>", help="Total level of a pascal triangle", type=int)
-    parser.add_argument("-k", "--row", required=True, metavar="<target row of interest>", help="Target row of interest", type=int)
+    parser.add_argument("-n", "--row", required=True, metavar="<nth row>", help="nth row", type=int)
     parser.add_argument("-l", "--log", help="Log level (DEBUG/INFO/WARNING/ERROR/CRITICAL)", type=str, default="INFO")
 
     args = parser.parse_args()
@@ -19,10 +18,9 @@ def main():
     args = command_line_args()
     logging.basicConfig(level=args.log)
 
-    logging.info("n: {}".format(args.number))
-    logging.info("k: {}".format(args.row))
+    logging.info("n: {}".format(args.row))
 
-    ret = pascal_triangle(args.number, args.row)
+    ret = pascal_triangle(args.row)
 
     logging.info("result: {}".format(ret))
 
